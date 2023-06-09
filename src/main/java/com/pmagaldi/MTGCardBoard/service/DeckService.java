bc8ae50f7e418b3518613903ff3d08e31d1c7070
Paulo Magaldi <42938392+pmagaldi@users.noreply.github.com>
2023-06-09 00:56:39 -0300

    começando a ajustar deckservice

--- src/main/java/com/pmagaldi/MTGCardBoard/service/DeckService.java
+++ src/main/java/com/pmagaldi/MTGCardBoard/service/DeckService.java
@@ -1,8 +1,31 @@
 package com.pmagaldi.MTGCardBoard.service;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.pmagaldi.MTGCardBoard.DTO.DeckDTO;
+import com.pmagaldi.MTGCardBoard.entities.Deck;
+import com.pmagaldi.MTGCardBoard.exceptions.BusinessException;
+import com.pmagaldi.MTGCardBoard.repository.DeckRepository;
 
 @Service
 public class DeckService {
     
+    @Autowired
+    DeckRepository deckRepository;
+
+    @Transactional(readOnly = true)
+    public List<DeckDTO> findAll() throws BusinessException{
+
+        List<Deck> decks = deckRepository.findAll();
+
+        if (decks.isEmpty()){
+            throw new BusinessException("Don't have any card in your collection");
+        }
+
+        return decks.stream().map(DeckDTO::new).toList();
+    }
 }
