9fc5d0ed288ad13ff1383d3b3029cfe8cd1492b2
Paulo Magaldi <paulo.magaldi@targetwork.com.br>
2023-06-09 02:14:30 -0300

    ajustes no card controller iniciando o mapping

--- src/main/java/com/pmagaldi/MTGCardBoard/controller/CardController.java
+++ src/main/java/com/pmagaldi/MTGCardBoard/controller/CardController.java
@@ -1,10 +1,31 @@
 package com.pmagaldi.MTGCardBoard.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.pmagaldi.MTGCardBoard.DTO.CardDTO;
+import com.pmagaldi.MTGCardBoard.exceptions.BusinessException;
+import com.pmagaldi.MTGCardBoard.service.CardService;
 
 @RestController
 @RequestMapping(value = "/card")
 public class CardController {
     
+    @Autowired
+    private CardService cardService;
+
+    @GetMapping(value = "/all")
+    public ResponseEntity<List<CardDTO>> findAll(){
+        try{
+            List<CardDTO> result = cardService.findAll();
+            return ResponseEntity.ok().body(result);
+        } catch (BusinessException exception){
+            return ResponseEntity.internalServerError().build();
+        }
+    }
 }
--- src/main/java/com/pmagaldi/MTGCardBoard/service/DeckService.java
+++ src/main/java/com/pmagaldi/MTGCardBoard/service/DeckService.java
@@ -5,6 +5,7 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.pmagaldi.MTGCardBoard.DTO.DeckDTO;
 import com.pmagaldi.MTGCardBoard.entities.Deck;
