b4b902f9f0edf9021b2594296fecd7407d1522a5
Paulo Magaldi <paulomagaldi@gmail.com>
2023-06-15 00:02:20 -0300

    completando deck controller e ReadMe

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # MTG-CardBoard
 API CRUD to organize your collection of cards
+
+[![License](https://img.shields.io/bower/l/ma)](https://opensource.org/license/mit/)
+
+## Requirements
+
+For building and running the application you need:
+
+- [JDK 1.8](http://www.oracle.com/technetwork/java/javase/downloads/jdk8-downloads-2133151.html)
+- [Maven 3](https://maven.apache.org)
+
+## Running the application locally
+
+There are several ways to run a Spring Boot application on your local machine. One way is to execute the `main` method in the `de.codecentric.springbootsample.Application` class from your IDE.
+
+Alternatively you can use the [Spring Boot Maven plugin](https://docs.spring.io/spring-boot/docs/current/reference/html/build-tool-plugins-maven-plugin.html) like so:
+
+```shell
+mvn spring-boot:run
+```
+
+## Copyright
+
+Released under the Apache License 2.0. See the [LICENSE](https://github.com/pmagaldi/MTG-CardBoard/blob/main/LICENSE) file.
--- src/main/java/com/pmagaldi/MTGCardBoard/controller/DeckController.java
+++ src/main/java/com/pmagaldi/MTGCardBoard/controller/DeckController.java
@@ -1,10 +1,76 @@
 package com.pmagaldi.MTGCardBoard.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.pmagaldi.MTGCardBoard.DTO.DeckDTO;
+import com.pmagaldi.MTGCardBoard.exceptions.BusinessException;
+import com.pmagaldi.MTGCardBoard.service.DeckService;
 
 @RestController
 @RequestMapping(value = "/deck")
 public class DeckController {
     
+    @Autowired
+    private DeckService deckService;
+
+    @GetMapping(value = "/all")
+    public ResponseEntity<List<DeckDTO>> findAll(){
+        try{
+            List<DeckDTO> result = deckService.findAll();
+            return ResponseEntity.ok().body(result);
+        } catch (BusinessException exception) {
+            return ResponseEntity.internalServerError().build();
+        }
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<DeckDTO> findById(@PathVariable Integer id){
+        try{
+            DeckDTO result = deckService.findById(id);
+            return ResponseEntity.ok().body(result);
+        } catch (BusinessException exception) {
+            return ResponseEntity.internalServerError().build();
+        }
+    }
+
+    @PostMapping
+    public ResponseEntity<Void> postDeck(@RequestBody DeckDTO deck){
+        try{
+            deckService.postDeck(deck);
+            return ResponseEntity.ok().build();
+        } catch (BusinessException exception) {
+            return ResponseEntity.internalServerError().build();
+        }
+    }
+
+    @PatchMapping
+    public ResponseEntity<DeckDTO> updateDeck(@RequestBody DeckDTO deck){
+        try{
+            DeckDTO result = deckService.updateDeck(deck);
+            return ResponseEntity.ok().body(result);
+        } catch (BusinessException exception) {
+            return ResponseEntity.internalServerError().build();
+        }
+    }
+
+    @DeleteMapping
+    public ResponseEntity<Void> deleteDeck(@PathVariable Integer id){
+        try{
+            deckService.deleteDeck(id);
+            return ResponseEntity.ok().build();
+        } catch (BusinessException exception) {
+            return ResponseEntity.internalServerError().build();
+        }
+    }
 }
